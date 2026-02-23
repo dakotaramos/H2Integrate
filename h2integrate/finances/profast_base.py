831a8b8a7cc5421d0e8d3b58a9ca40f6765f0299
Jonathan Martin <94018654+jmartin4u@users.noreply.github.com>
2026-02-23 09:59:04 -0700

    Iron: Adding electrowinning capabilities (#432)
    * Reading in Stinn input tables
    
    * Sucessfully testing Stinn model
    
    * Making humbert perf model
    
    * Adding humbert/stinn cost model inputs only
    
    * Progress on humbert cost
    
    * Humbert Stinn complete, not debugged
    
    * Debugging electrowinning
    
    * Debugging ewinning
    
    * Electrowinning cases running, need to add DRI case
    
    * Cleanup of old trailing underscores
    
    * Fix input csv names
    
    * Integrate ewin example with feedstocks
    
    * Moving functions out of h2i module and renumbering example
    
    * Small fixes to PR
    
    * start of unit test
    
    * Testing example
    
    * Expanding unit tests
    
    * fix import
    
    * Docstrings begun
    
    * Docstrings plus a little reorg
    
    * Everything but the docs page
    
    * Finished documentation
    
    * Split off humbert cost_model
    
    * Config docstrings
    
    * Refactoring iron ewinning calcs for clarity
    
    * Addressing reviews
    
    * site -> sites
    
    * Updating test values
    
    * Added autodoc to the electrowinning docs
    
    * updated iron electrowinning tech config
    
    * updated plant config for electrowinning example
    
    * minor udpates to cost model
    
    * minor updates to cost and performance models
    
    * fixed initialization error in cost model
    
    * removed duplicate costs and updated to use PerformanceModelBaseClass
    
    * updated example test values
    
    * Addressing elenya comments
    
    * feedstocks wip
    
    * updating integration
    
    * classic units
    
    * updated tech config to be compatible with develop
    
    * merged changelog
    
    * removed hard-coded cost numbers in compute() to config args with default values
    
    * Minor cleanup
    
    ---------
    
    Co-authored-by: Jonathan Martin <94018654+jmartin4nrel@users.noreply.github.com>
    Co-authored-by: John Jasa <johnjasa11@gmail.com>
    Co-authored-by: kbrunik <kbrunik@gmail.com>
    Co-authored-by: kbrunik <102193481+kbrunik@users.noreply.github.com>
    Co-authored-by: elenya-grant <116225007+elenya-grant@users.noreply.github.com>

--- h2integrate/finances/profast_base.py
+++ h2integrate/finances/profast_base.py
@@ -614,6 +614,7 @@ class ProFastBase(om.ExplicitComponent):
             "methanol",
             "iron_ore",
             "pig_iron",
+            "sponge_iron",
         ]
 
         # create years of operation list
